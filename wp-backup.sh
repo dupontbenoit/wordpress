3b1071de0bcb0eece0608574922ecd8eb56374fa
Benoit Dupont <dupont.benoit@gmail.com>
2019-05-12 18:58:34 +0200

    add backup/versioning script

---
+++ wp-backup.sh
@@ -0,0 +1,9 @@
+# Dump Wordpress Database
+# then commit the entire wp folder
+# the push to remote git repository
+
+cd /path/to/wp/folder/
+mysqldump --add-drop-table -the_password the_databse -u the_user > wp-db-backup.sql
+git add .
+git commit -a -m 'Autobackup'
+git push origin master
